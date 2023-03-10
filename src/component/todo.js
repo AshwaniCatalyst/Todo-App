82ff6b6105b1aa59bad7677f770eef67926a88cd
AshwaniCatalyst <ashwani.sood@catalyst.com>
2023-03-10 12:53:22 +0530

    designed basic structure

--- src/component/todo.js
+++ src/component/todo.js
@@ -4,8 +4,24 @@ import React from 'react'
 const Todo = () => {
   return (
     <>
-      <div>
-        
+      <div className="mainContiner">
+        <div className="childContainer flex">
+            <div className="appHeading flex">
+                <h1>Aaj Ka Kaam</h1>
+            </div>
+            <div  className="addItems flex">
+                <div className="inputTextWrapper flex ">
+                <input type="text" className="textField" placeholder="Add Title"/> 
+                <input type="text" className="textField" placeholder="Add Description"/>
+                </div>
+                <div className="buttonWrapper flex">
+                    <button>Add</button>
+                    <button>Delete All</button>
+                </div>
+
+            </div>
+        </div>
+
       </div>
     </>
   )
