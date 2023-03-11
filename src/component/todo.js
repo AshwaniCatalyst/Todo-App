1eae07e17b8a8851a12ddf7adfd7b8b1c0b25ea8
AshwaniCatalyst <ashwani.sood@catalyst.com>
2023-03-11 20:19:20 +0530

    save description's value and display it

--- src/component/todo.js
+++ src/component/todo.js
@@ -1,30 +1,63 @@
-import "./todo.css"
-import React from 'react'
+import "./todo.css";
+import React, { useState } from "react";
 
 const Todo = () => {
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputDescription, setInputDescription] = useState("");
+  const [items, setItems] = useState([]);
+  const addItem = () => {
+    if (!inputDescription) {
+    } else {
+      setItems([...items, inputDescription]);
+
+      setInputTitle("");
+      setInputDescription("");
+    }
+  };
   return (
     <>
       <div className="mainContiner">
         <div className="childContainer flex">
-            <div className="appHeading flex">
-                <h1>Aaj Ka Kaam</h1>
+          <div className="appHeading flex">
+            <h1>Aaj Ka Kaam</h1>
+          </div>
+          <div className="addItems flex">
+            <div className="inputTextWrapper flex ">
+              <input
+                type="text"
+                className="textField"
+                value={inputTitle}
+                onChange={(e) => setInputTitle(e.target.value)}
+                placeholder="Add Title"
+              />
+              <textarea
+                rows="4"
+                className="textField"
+                placeholder="Add Description"
+                value={inputDescription}
+                onChange={(e) => setInputDescription(e.target.value)}
+              />
             </div>
-            <div  className="addItems flex">
-                <div className="inputTextWrapper flex ">
-                <input type="text" className="textField" placeholder="Add Title"/> 
-                <input type="text" className="textField" placeholder="Add Description"/>
-                </div>
-                <div className="buttonWrapper flex">
-                    <button>Add</button>
-                    <button>Delete All</button>
-                </div>
-
+            <div className="buttonWrapper flex">
+              <button onClick={addItem}>Add</button>
+              <button>Delete All</button>
             </div>
+          </div>
+          <div className="showItems flex">
+            {}
+            {items.map((elem, ind) => {
+              return (
+                <div className="eachItem" key={ind}>
+                  <span className="deleteItem">X</span>
+                  <h2>{elem}</h2>
+                </div>
+              );
+            })}
+          </div>
         </div>
-
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
