1fe39b2f3a959737ca714982631579990f5447a9
AshwaniCatalyst <ashwani.sood@catalyst.com>
2023-03-16 23:51:40 +0530

    Added logic to insert description in the task card

--- src/component/DisplayTask/TaskCards.js
+++ src/component/DisplayTask/TaskCards.js
@@ -1,32 +1,32 @@
-import React from 'react'
-import '../todo.css'
-const TaskCards = ({Task, setTask}) => {
-  const deleteTask=(id)=>{
+import React from "react";
+import "../todo.css";
+const TaskCards = ({ Task, setTask }) => {
+  const deleteTask = (id) => {
     const updatedTasks = Task.filter((elem, index) => {
       return index !== id;
     });
     setTask(updatedTasks);
-  }
+  };
   return (
     <>
-    {
-        Task.map((task,id)=>{
-            return(
-                <div className="eachTask flex" key={id}>
-                  <span className="deleteTask">
-                    <i
-                      className="fa fa-close"
-                      onClick={() => deleteTask(id)}
-                    ></i>
-                  </span>
-                  <h2>{task} <span><input type="checkbox" /></span></h2>
-                </div>
-            )
-        })
-    }
-      
+      {Task.map((task, id) => {
+        return (
+          <div className="eachTask flex" key={id}>
+            <span className="deleteTask">
+              <i className="fa fa-close" onClick={() => deleteTask(id)}></i>
+            </span>
+            <h2>
+              {task.Title}
+              <span key={id}>
+                <input type="checkbox" />
+              </span>
+            </h2>
+            <h4>{task.Description}</h4>
+          </div>
+        );
+      })}
     </>
-  )
-}
+  );
+};
 
-export default TaskCards
+export default TaskCards;
--- src/component/TaskForm.js
+++ src/component/TaskForm.js
@@ -3,17 +3,39 @@ import InputField from './InputField/InputField'
 import '../component/todo.css'
 const TaskForm = ({Task, setTask}) => {
     const titleText= "Add Title";
+    const titleDescription = "Add Description";
     const[Title, setTitle] = useState('');
+    const[Description, setDescription]=useState('');
     const onFormSubmit=(event)=>{
          event.preventDefault();
-         setTask([...Task, Title])
+         if(!Title)
+         {
+          // alert("Please Add data!");
+         }
+         else{
+          let taskObject ={};
+          taskObject["Title"] = Title;
+          taskObject["Description"] = Description;
+         setTask([...Task, taskObject])
          setTitle('');
+         }
+    }
+    const deleteAllTask = ()=>{
+      if(Task.length===0)
+      {
+        alert("No task to Delete");
+      }
+      else{
+      setTask([]);
+      }
     }
   return (
     <form onSubmit={onFormSubmit} className="form flex">
       <InputField placeholder={titleText} Title={Title} setTitle={setTitle}/>
+      <InputField placeholder={titleDescription} Title={Description} setTitle={setDescription}/>
       <div className="buttonWrapper flex">
       <button type='submit'>Add Task</button>
+      <button onClick={()=>deleteAllTask()}>Reset</button>
       </div>
     </form>
   )
